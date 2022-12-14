de52d4682066662317d19fd8f97bc54b68acd4a6
root <root@ip-172-31-88-102.ec2.internal>
2022-12-14 16:19:21 +0000

    first commit

---
+++ code.java
@@ -0,0 +1,15 @@
+package suneel.example;
+
+public class sample {
+
+	public static void main(String[] args) {
+      int First = 4;
+      int Second = 6;
+      
+      // add two numbers
+      int sum = First + Second;
+      System.out.println(First + " + " + Second + " = " + sum);
+
+	}
+
+}
